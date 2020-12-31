ebd490b409e1e367b100ee3fcb67814371426d74
DESKTOP-B4I60VO\heybys <byn0174@gmail.com>
2020-12-31 21:29:55 +0900

    Solve algorithm, 10818_최소최대

---
+++ 10818_최소최대/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int N = scan.nextInt();
+		int max = 0;
+		int min = 0;
+
+		for (int i = 0; i < N; i++) {
+			int n = scan.nextInt();
+
+			if (i == 0) {
+				max = n;
+				min = n;
+			} else if (n < min) {
+				min = n;
+			} else if (n > max) {
+				max = n;
+			}
+		}
+
+		System.out.println(min + " " + max);
+	}
+
+}
